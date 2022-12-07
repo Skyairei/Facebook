df3cb565de087c85c5150f635af83b19c8f45751
Skyairei <mailofairey@gmail.com>
2022-12-07 23:44:07 +0530

    Updated

--- File.js
+++ File.js
@@ -1,2 +1,14 @@
-let reg_email = mailofairey@gmail.com
-let reg_pass = deffonottellingyou
+let num = 13
+    let i = 1;
+    let count = 0;
+    while(i<=n){
+        if(num%i===0){
+            count++;
+        }
+        i++;
+    }
+    if(count===2){
+        console.log("Yes");
+    }else{
+        console.log("No")
+    }
